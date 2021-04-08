1c651675248c829125d5953d0ff686204c2860d4
sitdikov_denis <fomk@mail.ru>
2021-04-08 15:43:24 +0300

    сделал фильтрацию в опросах для обычных пользователей

--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -1,3 +1,6 @@
+import datetime
+
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Poll, Question
@@ -9,6 +12,20 @@ class PollModelViewSet(ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollModelSerializer
     permission_classes = (PollPermission, )
+
+    def list(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            queryset = self.filter_queryset(self.get_queryset())
+        else:
+            queryset = Poll.objects.filter(end_date__gte=datetime.date.today())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class QuestionModelViewSet(ModelViewSet):
